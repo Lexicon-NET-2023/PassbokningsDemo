0a812bd3811092f70d12a17cadf3d6f77f3cd8d8
Jonathan Krall Olsson <jonathan.krall@lexicon.se>
2023-12-06 13:46:42 +0100

    Add project files.

---
+++ PassbokningsDemo/Models/ApplicationUserGymClass.cs
@@ -0,0 +1,12 @@
+﻿namespace PassbokningsDemo.Models
+{
+    public class ApplicationUserGymClass
+    {
+        public string ApplicationUserId { get; set; }
+
+        public int GymClassId { get; set; }
+
+        public ApplicationUser User { get; set; }
+        public GymClass GymClass { get; set; }
+    }
+}
